b1f5f5de57434a78f6cfcd9a0ae4c0b658eefbc5
Rainier Geron P. Aganan <111546490+rain-aganan@users.noreply.github.com>
2023-05-13 15:05:04 +0800

    Update Main.java

--- LABORATORY ACTIVITY 3/Main.java
+++ LABORATORY ACTIVITY 3/Main.java
@@ -1,1 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Shape rec = new Rectangle();
+        Shape tri = new Triangle();
+        Shape cir = new Circle();
+        Shape tra = new Trapeziod();
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input Shape Number : ");
+        System.out.println(" [1] = Rectangle ");
+        System.out.println(" [2] = Triangle ");
+        System.out.println(" [3] = Circle ");
+        System.out.println(" [4] = Trapeziod ");
+        int shape = input.nextInt();
+        
+        switch (shape){
+            case 1:
+                rec.rectangle();
+            break;
+            case 2:
+                tri.triangle();
+            break;
+            case 3:
+                cir.circle();
+            break;
+            case 4:
+                tra.trapeziod();
+            break;
+        }
+        
+    }
+}
 
